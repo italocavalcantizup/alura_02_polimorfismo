71c12518946bb8d1233c64eee606fe53334fc0f5
Italo Cavalcanti <italo.cavalcanti@zup.com.br>
2022-09-14 13:25:48 -0300

    criando composicoes e refatorando codigo

--- bytebank-herdado/src/Cliente.java
+++ bytebank-herdado/src/Cliente.java
@@ -1,16 +1,11 @@
-public class Administrador extends Funcionario implements Autenticavel {
-	
+public class Cliente implements Autenticavel {
+
 	private AutenticacaoUtil autenticador;
 	
-	public Administrador() {
+	public Cliente() {
 		this.autenticador = new AutenticacaoUtil();
 	}
 	
-	@Override
-	public double getBonificacao() {
-		return 50;
-	}
-
 	@Override
 	public void setSenha(int senha) {
 		this.autenticador.setSenha(senha);
