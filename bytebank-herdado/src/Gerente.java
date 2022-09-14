e94928ece65850393d5697fc36c5f7a3156253fa
Italo Cavalcanti <italo.cavalcanti@zup.com.br>
2022-09-14 12:51:12 -0300

    criando classe intermediaria e adicionando sistemaInterno para autenticacao

--- bytebank-herdado/src/Gerente.java
+++ bytebank-herdado/src/Gerente.java
@@ -1,17 +1,6 @@
-public class Gerente extends Funcionario {
-	private int senha;
+public class Gerente extends FuncionarioAutenticavel {
 	
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-	
-	public boolean autentica(int senha) {
-		if(this.senha == senha) {
-			return true;
-		}
-		return false;
-	}
-	
+	@Override
 	public double getBonificacao() {
 		return super.getSalario();
 	}
---
+++ bytebank-herdado/src/SistemaInterno.java
@@ -0,0 +1,14 @@
+public class SistemaInterno {
+
+	private int senha = 2222;
+	
+	public void autentica(FuncionarioAutenticavel fa) {
+		boolean autenticou = fa.autentica(this.senha);
+		if (autenticou) {
+			System.out.println("Acesso liberado.");
+		} else {
+			System.out.println("Acesso não permitido.");			
+		}
+	}
+	
+}
